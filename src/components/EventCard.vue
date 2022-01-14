7b0b64ebf6f31d6d1202c86d2ded1faa2d2a12fe
shadmanAh <shadman061@gmail.com>
2022-01-14 19:19:06 +0330

    add service file

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
@@ -13,19 +13,8 @@
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: "Beach Cleanup",
-        date: "Tues Aug 19, 2020",
-        time: "6:00",
-        attendees: [
-          { id: "abc123", name: "Shadman Ahmadi" },
-          { id: "efg456", name: "Hadi eshaghi" },
-        ],
-      },
-    };
+  props: {
+    event: Object,
   },
 };
 </script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
     <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventServices";
 
 export default {
   components: {
     EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.log("There was an error:" + error.response);
+      });
   },
 };
 </script>
