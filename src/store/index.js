7729b29f93bdc4d261780b82b86ceb0d8faf5e12
shadmanAh <shadman061@gmail.com>
2022-01-15 11:08:53 +0330

    complite EventCreate

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import EventService from "../services/EventServices.js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -15,15 +15,20 @@ export default new Vuex.Store({
       "food",
       "community",
     ],
-    events: [
-      { id: 1, text: "...", done: true },
-      { id: 2, text: "...", done: false },
-      { id: 3, text: "...", done: true },
-      { id: 4, text: "...", done: false },
-    ],
+    events: [],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event);
+    },
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit("ADD_EVENT", event);
+      });
+    },
+  },
   modules: {},
   getters: {
     getEventById: (state) => (id) => {
